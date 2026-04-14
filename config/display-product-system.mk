LineageOS/android_vendor_qcom_opensource_display-commonsys-intf

config: Add IDisplayConfig support for SetHdrCapabilities
Add IDisplayConfig support for SetHdrCapabilities in new version

Change-Id: Ia5e76c902c9d0ef511cf0e404ea2408f61596b80

config/display-product-system.mk
@@ -19,4 +19,5 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V13-ndk \
                     vendor.qti.hardware.display.config-V14-ndk \
                     vendor.qti.hardware.display.config-V15-ndk \
+                    vendor.qti.hardware.display.config-V16-ndk \
                     vendor.qti.hardware.display.snapallocext-V1-ndk

CodeLinaro-mirror/femto5g_platform_vendor_qcom-opensource_display-commonsys-intf

config/display-product-system.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V12-ndk \
                     vendor.qti.hardware.display.config-V13-ndk \
                     vendor.qti.hardware.display.config-V14-ndk \
-                    vendor.qti.hardware.display.config-V15-ndk
+                    vendor.qti.hardware.display.config-V15-ndk \
+                    vendor.qti.hardware.display.config-V16-ndk
 
 $(call soong_config_set, qtidisplaycommonsysintf, multi_view_support, false)